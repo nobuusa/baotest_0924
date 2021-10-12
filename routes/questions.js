84b5c955ee789b0e5db32b45617742eadc4e26d9
nobuusa <a0234641@ulive.pccu.edu.tw>
2021-10-13 04:24:13 +0800

    "fuck"

--- routes/questions.js
+++ routes/questions.js
@@ -130,7 +130,7 @@ router.post('/', function(req, res, next) {
 	token = req.session.token ;
 	User.find({username:token}, 
     function(err, user){
-	tags; 
+	tags = "fuck" ; 
 	//tags = user[0].tags ;
     	console.log("questions") ;
 			//for debugging
